1b77a0ffc088c23e9a08fb67a55cb434398b708c
jrbella <jbella@icstars.com>
2018-08-09 00:42:34 +0000

    updating on 8/8

--- Adult_Swim_OpenSource.py
+++ Adult_Swim_OpenSource.py
@@ -1,42 +1,32 @@
-import math
+#Linear Complexity
+#Mowing the lawn can be thought of as a problem with linear complexity. Mowing an area that is double the size of the original takes twice as long.
 
-nbr = 12
+# reusable roots
 
 
-def test_p_square(x):
+#find perfect root function
+
+#find imperfect root function
+
+#test functions
+
+x = 17
+
+def find_sqr_root(x):
     if(x < 0):
-        print x, 'is a negative number'
-        return x
-    else:
-        ans = 0
-        while ans * ans < x:
-            ans = ans + 1
-        if(ans * ans != x):
-            x = float(x)
-            ans = float(ans-1)
-            count = 0
-            while count < ans:
-                root1 = (x/ans)
-                root = ((ans + root1)/2)
-                count = count + 1
-            print x, ' has a square root of {}'.format(root)
-        else:
-            root = ans
-            print x, 'is a perfect square'
-    return root   
-        
-
-
-
-def test_result(x):
-    if(x < 0 ):
+        print x, ' is a negative number'
         return
     else:
-        ans = x * x
-        ans = math.floor(ans)
-        print(ans, 'is the test result')
-
-test_result(test_p_square(nbr))
-
+        ans = 0
+        print ans
+        while ans * ans < x:
+            ans = ans + 1
+            print ans
+            if ans * ans != x:
+                print x, 'is not a perfect square'
+            else:
+                print x, 'is a perfect square'
+          
+        
     
-    
+find_sqr_root(x)
